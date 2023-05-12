38dc6e1d2f0f07f78607d2d3ed05948a3b8ae41a
adamcastle <castle.adam@gmail.com>
2023-05-12 17:51:49 +1200

    Add files via upload

---
+++ con8.js
@@ -0,0 +1,78 @@
+import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+// define the component
+export class HelloWorld extends LitElement {
+  
+    
+  static properties = {
+    who: {type: String},
+  };
+    
+    static getData() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        },
+        me: {
+          type: 'string',
+          title: 'me',
+          description: 'Who to say hello to'
+        }
+      }
+    };
+  }
+
+  
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
+        who: {
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
+        },
+        me: {
+          type: 'string',
+          title: 'me',
+          description: 'Who to say hello to'
+        },
+        you: {
+          type: 'string',
+          title: 'me',
+          description: 'Who to say hello to'
+        }
+      }
+    };
+  }
+  
+  constructor() {
+	  debugger;
+    super();
+    this.who = 'World';
+  }
+
+  render() {
+	  debugger;
+	  
+    return html`
+	<img onload="alert('init');" src=''>${this.who} - ${this.me}
+	<body onload="alert('init');">${this.who} - ${this.me}
+	<input type='button' value="a" onclick="alert('text click');">
+	<input type='button' value="b" onclick="alert('text click');">
+	`;
+  }
+}
+
+// registering the web component
+const elementName = 'hello-world';
+customElements.define(elementName, HelloWorld);
