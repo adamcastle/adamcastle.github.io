eaea6be15a1385a9eed2801ea3a04253c31e9f96
adamcastle <castle.adam@gmail.com>
2023-05-25 08:56:36 +1200

    Add files via upload

--- laketree-gridjs.js
+++ laketree-gridjs.js
@@ -1,102 +1,22 @@
-
 import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 // define the component
-export class LakeTree_GridJS extends LitElement {
-
+export class HelloWorld extends LitElement {
+  
   static properties = {
-    firstname: {type: String},
-    jsonData: {type: String},
+    who: {type: String},
   };
   
-  gridScript() {
-    
-    let script = document.createElement('script');
-    script.onload = this.onLoad.bind(this);
-    script.src = 'https://unpkg.com/gridjs-jquery/dist/gridjs.production.min.js';
-
-    return script;
-  }
-
-  jQueryScript() {
-    
-    let script = document.createElement('script');
-    script.onload = this.onLoad.bind(this);
-    script.src = 'https://unpkg.com/jquery/dist/jquery.min.js';
-
-    return script;
-  }
-
-  kendoStyles() {
-    
-    let style = document.createElement('link');
-    style.onload = this.onLoad.bind(this);
-    style.rel = "stylesheet";
-    style.type = "text/css";
-    style.href = 'https://unpkg.com/gridjs/dist/theme/mermaid.min.css';
-
-    return style;
-  }
-
-  onLoad() {
-    console.log("onLoad");
-
-    debugger;
-
-        $.getScript('https://unpkg.com/gridjs-jquery/dist/gridjs.production.min.js', function(){
-
-        debugger;
-
-          $($('laketree-grid')[0].renderRoot.querySelector('#grid')).Grid({
-            columns: ['Name', 'Age', 'Email'],
-            data: [
-              ['John', 25, 'john@k.com'],
-              ['Mark', 59, 'mark@e.com']
-            ],
-          });
-    
-
-        });
-
-
-
-
-    // this.grid = new In({
-    //   columns: [{
-    //     id: 'columnheader1',
-    //     name: this.ColumnHeader1
-    //   }, {
-    //     id: 'columnheader2',
-    //     name: this.ColumnHeader2
-    //   }, {
-    //     id: 'columnheader3',
-    //     name: this.ColumnHeader3
-    //   }, {
-    //     id: 'columnheader4',
-    //     name: this.ColumnHeader4
-    //   }],
-    //   data: JSON.parse(this.Data),
-    //   sort: this.sortable
-    // });
-        
-  }
-
   // return a promise for contract changes.
   static getMetaConfig() {
     return {
-      controlName: 'LakeTree Grid JS',
+      controlName: 'Hello World',
       fallbackDisableSubmit: false,
       version: '1.2',
       properties: {
-        firstname: {
+        who: {
           type: 'string',
-          title: 'firstname',
+          title: 'Who',
           description: 'Who to say hello to'
-        }
-      },
-      properties: {
-        jsonData: {
-          type: 'string',
-          title: 'jsonData'
         }
       }
     };
@@ -104,23 +24,14 @@ export class LakeTree_GridJS extends LitElement {
   
   constructor() {
     super();
-    console.log("constructor");
-    this.firstname = 'Adam';
+    this.who = 'World';
   }
 
   render() {
-    console.log("render");
-    return html`
-    ${this.jQueryScript()}	
-    ${this.gridScript()}	
-    ${this.kendoStyles()}	
-    <div id="grid">this is a grid</div>
-    `;
-
+    return html`<p>Hello ${this.who}<p/>`;
   }
-
 }
 
 // registering the web component
-const elementName = 'laketree-gridjs';
-customElements.define(elementName, LakeTree_GridJS);
+const elementName = 'hello-world';
+customElements.define(elementName, HelloWorld);
